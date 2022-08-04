ccbc19a778461379f44438adc97477cfdcd7774a
lwooler <48956441+LWooler@users.noreply.github.com>
2022-08-03 22:47:50 -0500

    created artwork model

---
+++ Models/Artwork.cs
@@ -0,0 +1,13 @@
+﻿namespace Art_Gallery_API.Models
+{
+    public class Artwork
+    {
+        public int Id { get; set; }
+        public string Path { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public ICollection<string> Medium { get; set; }
+        public ICollection<string> Subject { get; set; }
+        public string Size { get; set; }
+    }
+}
