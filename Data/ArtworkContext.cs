f12231d02cab90bd1138b4bb46bc4c868daf45ac
lwooler <48956441+LWooler@users.noreply.github.com>
2022-08-03 22:55:48 -0500

    created artworkcontext

---
+++ Data/ArtworkContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Art_Gallery_API.Models;
+
+namespace Art_Gallery_API.Data
+{
+    public class ArtworkContext : DbContext
+    {
+        public ArtworkContext(DbContextOptions<ArtworkContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Artwork> Artworks { get; set; }
+    }
+}
